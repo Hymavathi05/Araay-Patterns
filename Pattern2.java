d7f97e73045aba0ce16ace71bc1f72115ec4b811
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-24 16:25:26 +0530

    Implement square star pattern using nested loops
    Created a square star pattern using nested loops.
    This helped reinforce understanding of row and column traversal
    and how nested loops control two-dimensional output.

---
+++ Pattern2.java
@@ -0,0 +1,20 @@
+package com.pack1;
+
+public class Pattern2 {
+       public void pattern2()
+       {
+    	   int n=3;
+    	   for(int i=1;i<=n;i++)
+    	   {
+    		   System.out.print("* ");
+    	   }
+       }
+	public static void main(String[] args) {
+		Pattern2 p2=new Pattern2();
+		p2.pattern2();                /*
+		                               Output:
+		                               * * * 
+		                               */
+	}
+
+}
