a926127ace6b5c71c12c20dd370a6f843ec7b197
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-24 16:33:18 +0530

    Implement incremental number triangle pattern
    Created a number-based triangle pattern where each row
    prints increasing numbers starting from 1.
    This problem focuses on loop control and number-based output
    instead of characters.

---
+++ Pattern10.java
@@ -0,0 +1,21 @@
+package com.pack1;
+public class Pattern10 {
+   public void pattern10()
+   {
+	   int n=5;
+	   for(int i=1;i<=n;i++)
+	   {
+		   for(int j=1;j<=i;j++)
+		   {
+			   System.out.print(j + " ");
+		   }
+		   System.out.println();
+	   }
+   }
+	public static void main(String[] args) {
+		Pattern10 p10=new Pattern10();
+		p10.pattern10();
+
+	}
+
+}
